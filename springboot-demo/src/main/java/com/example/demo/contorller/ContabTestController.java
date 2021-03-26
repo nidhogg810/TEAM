c9b7dc71d671ad69930fff9af5d5c91a34e7ed66
nidhogg810 <yhnidhogg@gmail.com>
2021-03-26 10:33:05 +0800

    springboot启用TLS/SSL

--- springboot-demo/src/main/java/com/example/demo/contorller/ContabTestController.java
+++ springboot-demo/src/main/java/com/example/demo/contorller/ContabTestController.java
@@ -5,13 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class ContabTestController {
     @Autowired
     private CrontabUtil crontabUtil;
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     @RequestMapping("cron")
     public String parseCrontab(){
-        String cron = "0 0/5 * * * ?";
-        return crontabUtil.parseCon(cron).toString();
+//        String cron = "0 0/5 * * * ?";
+//        return crontabUtil.parseCon(cron).toString();
+        Date date = new Date();
+        return sdf.format(date);
+
     }
 }
