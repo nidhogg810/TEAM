3bbc12ef2554784a0ccf8b14f8ab682f04cd5032
nidhogg810 <yhnidhogg@gmail.com>
2021-04-28 11:21:52 +0800

    修复对于复杂POJO中复杂类型property的内容
    增加对property为其他POJO组合的方式的判断,
    增加判断其是否是基本数据类型、包装类、或者String,如果不是则进行递归

--- springboot-demo/src/main/java/com/example/demo/common/utils/MapUtils.java
+++ springboot-demo/src/main/java/com/example/demo/common/utils/MapUtils.java
@@ -3,6 +3,7 @@ package com.example.demo.common.utils;
 
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
@@ -13,8 +14,16 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 public class MapUtils {
+    /**
+     * 将简单的POJO转化为map，
+     * key的名字为对应的property名
+     * 注意如果没有public的get方法，则没有对应map字段
+     * @param obj 需要转化的POJO对象
+     * @return 转化结果的Map对象
+     * @throws Exception
+     */
     public static Map<String, Object> objectToMap(Object obj) throws Exception {
         if(obj == null) {
             return null;
@@ -30,12 +39,29 @@ public class MapUtils {
             }
             Method getter = property.getReadMethod();
             Object value = getter!=null ? getter.invoke(obj) : null;
+            if(!isPrimitive(value)){
+                value = objectToMap(value);
+            }
             map.put(key, value);
         }
 
         return map;
     }
 
+    /**
+     * 将map转化为指定类的对象，
+     * key的名字为对应的property名
+     * 注意如果对应字段没有public的set方法则对应属性为默认值
+     * @param map 需要转化的Map对象
+     * @param classz 目标的POJO的class
+     * @param <T>
+     * @return
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IntrospectionException
+     */
     public static <T> T map2Object(Map<String,Object> map,Class<T> classz) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, IntrospectionException {
 //        ObjectMapper mapper = new ObjectMapper();
 //        return mapper.convertValue(map, classz);
@@ -49,10 +75,80 @@ public class MapUtils {
                 continue;
             }
             Method setter = property.getWriteMethod();
-            if(setter != null){
+            if(setter != null&&isPrimitive(property.getPropertyType())){
                 setter.invoke(obj,map.get(key));
+            }else if(setter!=null){
+                setter.invoke(obj,map2Object((Map<String, Object>) map.get(key),property.getPropertyType()));
             }
         }
         return obj;
+    }
+
+    /**
+     * 根据指定的XXX.XXX.XXX递归的访问嵌套map的指定value
+     * @param map
+     * @param key
+     * @return
+     */
+    public static Object get(Map<String,Object> map, String key){
+        return get(map,key,"\\.");
+    }
+
+
+    /**
+     * 根据指定的XXX.XXX.XXX递归的访问嵌套map的指定value,key path的分割符可以任意指定
+     * @param map
+     * @param key
+     * @param splitChar 分割符
+
+     * @return
+     */
+    public static Object get(final Map<String,Object> map, String key, String splitChar){
+        Object result = null;
+        String[] keys = key.split(splitChar);
+        Map<String,Object> tmp = map;
+        for(int i=0; i<keys.length; i++){
+            Object nowValue = tmp.get(keys[i]);
+            if(i != keys.length-1){
+                if(nowValue instanceof Map){
+                    tmp = (Map<String, Object>) nowValue;
+                    continue;
+                }else {
+                    log.error("input key path [{}] error! cannot find key[{}] in Map[{}]",key,keys[i],map);
+                    throw new IllegalArgumentException("input key path ["+key+"] error! cannot find key["+keys[i]+"] in Map["+map+"]");
+                }
+            }else {
+                if (nowValue == null) {
+                    log.error("input key path [{}] error! cannot find key[{}] in Map[{}] or value is null",key,keys[i],map);
+                    throw new IllegalArgumentException("input key path ["+key+"] error! cannot find key["+keys[i]+"] in Map["+map+"]");
+                }else {
+                    result = nowValue;
+                }
+            }
+
+        }
+        return result;
+    }
+
+    public static boolean isPrimitive(Class clazz) throws NoSuchMethodException {
+        if(clazz.isPrimitive()||clazz == String.class){
+            return true;
+        }
+        try {
+            return ((Class<?>) clazz.getField("TYPE").get(null)).isPrimitive();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isPrimitive(Object obj) {
+        if(obj instanceof String){
+            return true;
+        }
+        try {
+            return ((Class<?>) obj.getClass().getField("TYPE").get(null)).isPrimitive();
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
