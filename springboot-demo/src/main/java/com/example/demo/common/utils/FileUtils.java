d5251d4bdb461c66b4ef20c708162c64e5fa9870
nidhogg810 <yhnidhogg@gmail.com>
2021-05-11 10:42:59 +0800

    新增xml解析器和yaml解析器

--- springboot-demo/src/main/java/com/example/demo/common/utils/FileUtils.java
+++ springboot-demo/src/main/java/com/example/demo/common/utils/FileUtils.java
@@ -1,2 +1,18 @@
-package com.example.demo.common.utils;public class FileUtils {
+package com.example.demo.common.utils;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+/**
+ * @author yuhan
+ */
+public class FileUtils {
+    public static Map<String,Object> loadYamlFile(String filepath) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        Object yamlParse = yaml.load(new FileInputStream(filepath));
+        return (Map<String, Object>) yamlParse;
+    }
 }
--- springboot-demo/src/main/java/com/example/demo/common/utils/xml/XmlParseHelper.java
+++ springboot-demo/src/main/java/com/example/demo/common/utils/xml/XmlParseHelper.java
@@ -1,18 +1,18 @@
-package com.example.demo.common.xml;
+package com.example.demo.common.utils.xml;
 
-import com.example.demo.test.TestJAXBDataRootConfigs;
+
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
-public class xmlParseHelper {
+public class XmlParseHelper {
     public static <T> T JAXBParseXml(Class<T> clazz,String filepath) throws JAXBException {
        return JAXBParseXml(clazz,new File(filepath));
     }
     public static <T> T JAXBParseXml(Class<T> clazz,File file) throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(TestJAXBDataRootConfigs.class);
+        JAXBContext context = JAXBContext.newInstance(clazz);
         Unmarshaller jaxbUnmarshaller = context.createUnmarshaller();
         return (T) jaxbUnmarshaller.unmarshal(file);
     }
